4cb942131d0ac0938b85f5aa666fdd562f7d300f
FuHao <fuhao_1207@163.com>
2022-06-14 19:24:46 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author FH
+ * @create 2022/6/14 18:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
